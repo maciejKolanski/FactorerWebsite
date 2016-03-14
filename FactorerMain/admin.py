9c3b8121b2533061b534ca0b37a59f589737fdd5
Rafal Duraj <Rafal Duraj>
2016-03-14 18:51:43 +0100

    Konto admina zrobione, można teraz wejsc na adres/admin i z tej strony można dodawać rzeczy do bazy.

--- FactorerMain/admin.py
+++ FactorerMain/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User
+from .models import Task
+from .models import Algorithm
+from .models import Element
+
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(Algorithm)
+admin.site.register(Element)
